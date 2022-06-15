2d2fa38e88982100ee2132a0b762a79eace5fc3f
cdolly <72312670+cdolly@users.noreply.github.com>
2022-06-15 10:38:09 -0400

    Update main.py

--- DecisionTree/main.py
+++ DecisionTree/main.py
@@ -1,1 +1,17 @@
+# Machine Learning with Python
+# Created by Cameron Dolly
+# Goal is to create a simulation of what using a decision tree to formulate a chouce using an example data set.
+# The idea for this project was obtained through the internet, though the coding and work are my own
 
+import pandas as pd
+from sklearn import tree
+import pydotplus
+from sklearn.tree import DecisionTreeClassifier
+import matplotlib.pyplot as plt
+import matplotlib.image as pltimg
+# Necessary imports for this project
+
+
+df = pandas.read_csv("shows.csv")
+
+print(df)
